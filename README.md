a79346ba2c29ae5b45d1aba8594ffd8ac1a65c01
Alderaeney <alderaeney@gmail.com>
2022-05-19 14:52:43 +0200

    Added dev and prod modes for docker build

--- README.md
+++ README.md
@@ -7,7 +7,12 @@ It uses the Next.js framework and MongoDB for storing data.
 
 Chapters uploaded to the server will be decompressed and then, the images will be converted to ".jpg".
 
-## How to run the application
+## How to run the application in production
 
-- Edit docker-compose.yaml args
-- Run `bash build.sh`
+- Edit `docker/prod/.env` env variables
+- Run `bash build.sh prod`
+
+## How to run the application in development
+
+- Edit `docker/dev/.env` env variables
+- Run `bash build.sh dev`
--- build.sh
+++ build.sh
@@ -4,22 +4,77 @@ _script="$(readlink -f ${BASH_SOURCE[0]})"
 
 directory="$(dirname $_script)"
 
-if [ -d mongo ]; then
-    sudo rm -r mongo
+if [ "$1" == "prod" ]; then
+    cd $directory
+
+    docker container stop mangaloidedb mangaloideapp
+
+    docker container rm mangaloideapp mangaloidedb
+
+    if [ -d mongo ]; then
+        sudo rm -r mongo
+    fi
+
+    sudo rm -r comics tmp .env
+
+    cd $directory/userimages
+
+    ls | grep -v default.jpg | sudo xargs rm -r
+
+    cd $directory
+
+    mkdir comics tmp
+
+    chmod -R 777 tmp comics userimages
+
+    cd $directory/docker/prod
+
+    cp .env $directory
+
+    docker-compose up -d --build
+
+    docker image prune -f
+
+    docker container prune -f
+
+elif [ "$1" == "dev" ]; then
+    cd $directory
+
+    docker container stop mangaloidedb mangaloideapp
+
+    docker container rm mangaloideapp mangaloidedb
+
+    if [ -d mongo ]; then
+        sudo rm -r mongo
+    fi
+
+    sudo rm -r comics tmp .env
+
+    cd $directory/userimages
+
+    ls | grep -v default.jpg | sudo xargs rm -r
+
+    cd $directory
+
+    mkdir comics tmp
+
+    chmod -R 777 tmp comics userimages
+
+    cd $directory/docker/dev
+
+    cp .env $directory
+
+    docker-compose up -d
+
+    docker image prune -f
+
+    docker container prune -f
+
+    cd $directory
+
+    yarn dev
+else
+    echo "build.sh [option]"
+    echo "prod - Build application for production"
+    echo "dev - Build application for development"
 fi
-
-sudo rm -r comics tmp
-
-cd $directory/userimages
-
-ls | grep -v default.jpg | sudo xargs rm -r
-
-cd $directory
-
-mkdir comics tmp
-
-chmod -R 777 tmp comics userimages
-
-docker-compose up -d --build
-
-docker image prune -f
